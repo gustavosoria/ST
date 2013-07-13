a82711c60eeb75a0a66bc39f6ba842668ce39a57
Gustavo G. Soria <gustavo.soria@me.com>
2013-07-13 18:16:08 +0200

    test11
    vulnerability 11: variable selectclass

---
+++ AddAssigment/AddAssignment.java
@@ -0,0 +1,70 @@
+package test;
+
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.InputElementFactory;
+import net.sourceforge.jwebunit.api.IElement;
+import net.sourceforge.jwebunit.htmlunit.HtmlUnitElementImpl;
+import net.sourceforge.jwebunit.junit.WebTester;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.helpers.AttributesImpl;
+
+/**
+ * @author Gustavo German Soria
+ * 
+ * File: AddAssignments.php 
+ * Variables: ‘selectclass’
+ * Vulnerability: the variable is passed within a hidden field, but the value can be modified closing  
+ * the tag and refining the attack.
+ */
+public class AddAssignment {
+    
+    private WebTester tester;
+    
+    public AddAssignment() {
+    }
+    
+    @Before
+    public void setUp() {
+        tester = new WebTester();
+        tester.setBaseUrl("http://localhost:8888/schoolmate/");
+    }
+
+    @Test
+    public void testVulnerability11() {
+        tester.beginAt("index.php");
+        tester.assertMatch("Today's Message");
+
+        tester.setTextField("username", "teacher1");
+        tester.setTextField("password", "nonlaso");
+        tester.submit();
+
+        tester.assertMatch("topolino topolino's Classes");
+        tester.setWorkingForm("teacher");
+        tester.clickLinkWithText("class1");
+
+
+        tester.assertMatch("Class Settings");
+        tester.clickLinkWithText("Assignments");
+        tester.assertMatch("Manage Assignments");
+        tester.clickButtonWithText("Add");
+
+        tester.setWorkingForm("addassignment");
+        tester.setTextField("selectclass", "4 '> <a href=www.ibm.com>malicious link</a> <br / '");
+        addSubmitButton("//form[@name='addassignment']");
+        tester.submit();
+
+        tester.assertLinkNotPresentWithText("malicious link");
+    }
+    
+    private void addSubmitButton(String expression) {
+        IElement element = tester.getElementByXPath(expression);
+        DomElement form = ((HtmlUnitElementImpl) element).getHtmlElement();
+        InputElementFactory factory = InputElementFactory.instance;
+        AttributesImpl attributes = new AttributesImpl();
+        attributes.addAttribute("", "", "type", "", "submit");
+        HtmlElement submit = factory.createElement(form.getPage(), "input", attributes);
+        form.appendChild(submit);
+    }
+}
