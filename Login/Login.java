7d95f594986d4ce9a0d32a5f178a2bd8c15f4ab9
Gustavo G. Soria <gustavo.soria@me.com>
2013-07-14 13:08:05 +0200

    update login.php with testcase 105

--- Login/Login.java
+++ Login/Login.java
@@ -10,9 +10,9 @@ import org.junit.Test;
  * ID: 163076
  * 
  * File: Login.php line 
- * Variable: $text [‘sitetext’ inside the database table]
+ * Variable: $text [‘sitetext’ inside the database table], $message
  * Vulnerability: the variable is directly taken from the database 
- * throught the simple query:  “select sitetext from schoolinfo”.
+ * throught the simple query like:  “select sitetext from schoolinfo”.
  * It positive because during the update, the variable is stored 
  * (in the database) and used (during the web session) without 
  * any kind of control, so it is liable to a xss attack.
@@ -20,7 +20,8 @@ import org.junit.Test;
 public class Login {
 
     private WebTester tester;
-    private String previousValue54 = " ";
+    private String previousValue54 = "mytext";
+    private String previousValue105 = "mymessage";
 
     public Login() {
     }
@@ -32,6 +33,7 @@ public class Login {
     }
 
     @Test
+    //variable text
     public void testVulnerability54() {
         tester.beginAt("index.php");
         tester.assertMatch("Today's Message");
@@ -42,8 +44,26 @@ public class Login {
         tester.assertMatch("Manage Classes");
         tester.clickLinkWithText("School");
         tester.assertMatch("Manage School Information");
-        previousValue54 = tester.getElementByXPath("html//textarea[@name='sitetext']").getTextContent();
+        //previousValue54 = tester.getElementByXPath("html//textarea[@name='sitetext']").getTextContent();
         tester.setTextField("sitetext", "<a href=http://www.ibm.com>Malicious link Text</a>");
+        tester.clickButtonWithText(" Update ");
+        tester.clickLinkWithText("Log Out");
+        tester.assertLinkNotPresentWithText("Malicious link Text");
+    }
+    @Test
+    //variable message
+    public void testVulnerability105() {
+        tester.beginAt("index.php");
+        tester.assertMatch("Today's Message");
+
+        tester.setTextField("username", "test");
+        tester.setTextField("password", "test");
+        tester.submit();
+        tester.assertMatch("Manage Classes");
+        tester.clickLinkWithText("School");
+        tester.assertMatch("Manage School Information");
+        //previousValue105 = tester.getElementByXPath("html//textarea[@name='sitemessage']").getTextContent();
+        tester.setTextField("sitemessage", "<a href=http://www.ibm.com>Malicious link Text</a>");
         tester.clickButtonWithText(" Update ");
         tester.clickLinkWithText("Log Out");
         tester.assertLinkNotPresentWithText("Malicious link Text");
@@ -61,6 +81,20 @@ public class Login {
         tester.clickLinkWithText("School");
         tester.assertMatch("Manage School Information");
         tester.setTextField("sitetext", this.previousValue54);
+        tester.clickButtonWithText(" Update ");
+    }
+    @After
+    public void tearDown105() {
+        tester.beginAt("index.php");
+        tester.assertMatch("Today's Message");
+
+        tester.setTextField("username", "test");
+        tester.setTextField("password", "test");
+        tester.submit();
+        tester.assertMatch("Manage Classes");
+        tester.clickLinkWithText("School");
+        tester.assertMatch("Manage School Information");
+        tester.setTextField("sitemessage", this.previousValue105);
         tester.clickButtonWithText(" Update ");
     }
 }
